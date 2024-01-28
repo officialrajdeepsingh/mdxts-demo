e7755e61aff78437476de7961824b59105e09ccf
Rajdeep Singh <radewala.2@gmail.com>
2024-01-28 18:44:47 +0530

    configure the Mdxts in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import { createMdxtsPlugin } from 'mdxts/next';
+
+const withMdxts = createMdxtsPlugin({
+    theme: 'nord'
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withMdxts(nextConfig);
